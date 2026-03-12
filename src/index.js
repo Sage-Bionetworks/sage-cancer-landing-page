726ef2b16709daee11a0cfcceb4787dbe2927721
Adam Hindman <adamhindman@gmail.com>
2026-03-11 17:03:24 -0700

    all done with the basics

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuOpen = document.getElementById("menuOpen");
-  const menuClose = document.getElementById("menuClose");
-  const mobileMenu = document.getElementById("mobileMenu");
+    const menuOpen = document.getElementById("menuOpen");
+    const menuClose = document.getElementById("menuClose");
+    const mobileMenu = document.getElementById("mobileMenu");
 
-  if (menuOpen && menuClose && mobileMenu) {
-    menuOpen.addEventListener("click", () => {
-      mobileMenu.classList.add("active");
-      document.body.style.overflow = "hidden"; // Prevent scrolling
-    });
+    if (menuOpen && menuClose && mobileMenu) {
+        menuOpen.addEventListener("click", () => {
+            mobileMenu.classList.add("active");
+            document.body.style.overflow = "hidden"; // Prevent scrolling
+        });
 
-    menuClose.addEventListener("click", () => {
-      mobileMenu.classList.remove("active");
-      document.body.style.overflow = ""; // Restore scrolling
-    });
+        menuClose.addEventListener("click", () => {
+            mobileMenu.classList.remove("active");
+            document.body.style.overflow = ""; // Restore scrolling
+        });
 
-    // Close menu when a link is clicked
-    const mobileLinks = mobileMenu.querySelectorAll("a");
-    mobileLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        mobileMenu.classList.remove("active");
-        document.body.style.overflow = "";
-      });
-    });
-  }
+        // Close menu when a link is clicked
+        const mobileLinks = mobileMenu.querySelectorAll("a");
+        mobileLinks.forEach((link) => {
+            link.addEventListener("click", () => {
+                mobileMenu.classList.remove("active");
+                document.body.style.overflow = "";
+            });
+        });
+    }
 });
+
+document
+    .getElementById("feedback-button")
+    ?.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const iframe = document.querySelector("#jsd-widget");
+        const iframeDoc =
+            iframe?.contentDocument || iframe?.contentWindow?.document;
+        const fab = iframeDoc?.querySelector("#help-button");
+
+        if (fab) {
+            fab.click();
+        }
+    });
